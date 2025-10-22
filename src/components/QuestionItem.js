72e186c2277de764ef1223ae598a7d1ea9c9d5a7
George <gnm7208@gmail.com>
2025-10-22 05:59:29 +0300

    Update QuestionItem.js

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, onDeleteQuestion }) {
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
@@ -8,6 +8,16 @@ function QuestionItem({ question }) {
       {answer}
     </option>
   ));
+
+  function handleDelete() {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    }).then((res) => {
+      if (res.ok) {
+        onDeleteQuestion(id);
+      }
+    });
+  }
 
   return (
     <li>
@@ -17,7 +27,7 @@ function QuestionItem({ question }) {
         Correct Answer:
         <select defaultValue={correctIndex}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDelete}>Delete Question</button>
     </li>
   );
 }
